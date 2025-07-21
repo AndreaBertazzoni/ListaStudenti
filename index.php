c8842acb470651f8831ac5a0aca823388498583f
AndreaBertazzoni <abertazzoni11@gmail.com>
2025-07-21 23:38:43 +0200

    primo commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+$students_list = array("Andrea", "Giada", "Federico", "Antonio");
+
+$attendance = [
+    "andrea" => [
+        1 => true,
+        2 => true,
+        3 => true,
+        4 => true,
+        5 => true,
+        6 => true,
+        7 => true,
+    ],
+    "giada" => [
+        1 => true,
+        2 => true,
+        3 => true,
+        4 => true,
+        5 => true,
+        6 => true,
+        7 => true,
+    ],
+    "federico" => [
+        1 => true,
+        2 => true,
+        3 => true,
+        4 => true,
+        5 => true,
+        6 => true,
+        7 => true,
+    ],
+    "antonio" => [
+        1 => true,
+        2 => true,
+        3 => true,
+        4 => true,
+        5 => true,
+        6 => true,
+        7 => true,
+    ],
+];
+
+$attendance_count = [
+    "andrea" => 0, 
+    "giada" => 0, 
+    "federico" => 0, 
+    "antonio" => 0, 
+];

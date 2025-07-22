163a9d5cfdd8a9b152828e2da7d097e640610673
AndreaBertazzoni <abertazzoni11@gmail.com>
2025-07-22 18:53:01 +0200

    v.0.1.00

--- index.php
+++ index.php
@@ -1,49 +1,43 @@
 <?php
 
-$students_list = array("Andrea", "Giada", "Federico", "Antonio");
+$presenze_studenti = ["Andrea Bertazzoni" => [], "Giada Altomare" => [], "Federico Carrassi" => [], "Antonino Beninato" => [],];
 
-$attendance = [
-    "andrea" => [
-        1 => true,
-        2 => true,
-        3 => true,
-        4 => true,
-        5 => true,
-        6 => true,
-        7 => true,
-    ],
-    "giada" => [
-        1 => true,
-        2 => true,
-        3 => true,
-        4 => true,
-        5 => true,
-        6 => true,
-        7 => true,
-    ],
-    "federico" => [
-        1 => true,
-        2 => true,
-        3 => true,
-        4 => true,
-        5 => true,
-        6 => true,
-        7 => true,
-    ],
-    "antonio" => [
-        1 => true,
-        2 => true,
-        3 => true,
-        4 => true,
-        5 => true,
-        6 => true,
-        7 => true,
-    ],
-];
+$numero_studenti = count($presenze_studenti);
 
-$attendance_count = [
-    "andrea" => 0, 
-    "giada" => 0, 
-    "federico" => 0, 
-    "antonio" => 0, 
-];
+$giorni = 7;
+
+$lista_presenze = array_fill_keys(range(1, $giorni), 0);
+
+
+for ($i = 1; $i <= $giorni; $i++) {
+    foreach ($presenze_studenti as $studente => $presenza) {
+        $random_bool = random_int(0, 1);
+        if ($random_bool) {
+            $presenze_studenti[$studente][] = $i;
+            $lista_presenze[$i]++;
+        }
+    }
+}
+
+foreach ($presenze_studenti as $studente => $presenza) {
+    echo "$studente:\n";
+    if (!count($presenza) == 0) {
+        echo "\tPresente nei giorni: " . implode(",", $presenza) . "\n";
+    }
+    echo "\t" . count($presenza) . " presenz" . ((count($presenza) === 1 ? 'a' : 'e')) . "\n";
+    echo "\tPresente tutti i giorni: ";
+    if (count($presenza) == $giorni) {
+        echo "SI\n\n";
+    } else {
+        echo "NO\n\n";
+    }
+}
+
+echo str_repeat("-", 50) . "\n\n";
+
+
+foreach ($lista_presenze as $giorno => $presenze) {
+    $presenti = $presenze;
+    $assenti = $numero_studenti - $presenti;
+    echo "Giorno $giorno: $presenti present" . ($presenti === 1 ? 'e' : 'i') . " e {$assenti} assent" . ($assenti === 1 ? 'e' : 'i') . " => \n";
+}
